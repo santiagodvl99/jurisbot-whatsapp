769870830aaad19ad5879755a581e5883f4c0397
santiagodvl99 <santiago.diaz.verbel.7.99@gmail.com>
2025-07-16 19:52:10 -0500

    main.py

--- main.py
+++ main.py
@@ -67,5 +67,8 @@ def get_gpt_response(user_input):
     result = response.json()
     return result['choices'][0]['message']['content']
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
